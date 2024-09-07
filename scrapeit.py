7d4d38313100f5b26a6a21dca07ba99189fc9470
Emiliano Sanchez <emiliano.sanchez3310@gmail.com>
2024-09-06 18:21:46 -0700

    fixing code loss error

--- scrapeit.py
+++ scrapeit.py
@@ -5,6 +5,9 @@ import webbrowser
 from bs4 import BeautifulSoup
 import requests
 import time
+import json
+import csv
+import pickle
 
 # You can change the end tags to whatever sources you want to receive in your web results.
 end_tags_coding = [': GeeksforGeeks', ': Stack Overflow', ': W3 Schools']
@@ -25,32 +28,70 @@ def home():
         app_icon = None,
         timeout = 3,
     )
- 
+
+    return render_template("scrapeit_home.html")
+
+
+@app.route("/search", methods=['POST', 'GET'])
+def search():
+
     if request.method == 'POST':
         URL = request.form['ui']
-
         try:
             response = requests.get(URL)
-            response.raise_for_status()  # Raise an exception for non-200 response codes
+            response.raise_for_status()
             soup = BeautifulSoup(response.content, 'html.parser')
 
-            show_data1 = None  # Initialize outside the loop
+            show_data1 = None
             lines = soup.find_all('p')
 
             scraped_data = [line.text.strip() for line in soup.find_all('p')]
+            data_texts.append(scraped_data)
 
             return render_template('result.html', scraped_data=scraped_data)
         except Exception as e:
             flash(f"An error occurred: {e}")
             return render_template('scrapeit_home.html')
 
-    return render_template("scrapeit_home.html")
+    return render_template("scrapeit_search.html")
+
+
+@app.route("/export_data", methods=['POST', 'GET'])
+def export_data():
+    if request.method == 'POST': 
+
+        button_name = request.form.get('button')
+        if button_name == 'export_to_json':
+            file_name = "scrape-it_data"
+            
+            with open(f"{file_name}.json", 'w') as json_file:
+                         json.dump(data_texts, json_file)
+
+            return render_template("success_export.html")
+            
+        elif button_name == 'export_to_csv':
+             with open('scrape-it_data.csv', 'w', newline='') as csvfile:
+                  csv_writer = csv.writer(csvfile)
+                  csv_writer.writerows(data_texts)
+
+             return render_template("success_export.html")
+        
+        else:
+             file = open('scrape-it_data', 'wb')
+
+             pickle.dump(data_texts, file)
+
+             file.close()
+             
+             return render_template("success_export.html")
+
+
+    return render_template("export_data.html")
 
 
 @app.route("/about")
 def about():
-    return render_template("scrapeit_about.html")
-
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
